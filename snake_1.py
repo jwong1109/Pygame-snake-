dbaae18a55c8fec564b4254639ea3be09c99bb72
jwong1109 <83558222+jwong1109@users.noreply.github.com>
2023-02-16 08:59:11 +1300

    Update snake_1.py

--- snake_1.py
+++ snake_1.py
@@ -2,8 +2,27 @@ import pygame
 import time
 pygame.init()
 
-screen = pygame.display.set_mode((1000, 720))
-time.sleep(5)
+screen = pygame.display.set_mode((1000, 750))
+game_icon = pygame.image.load('snake_icon.png')
+pygame.display.set_icon(game_icon)
+pygame.display.set_caption("Snake game - by Joseph Wong")
+
+# Tuples containing the colours to be used in the game
+black = (0, 0, 0)
+white = (255, 255, 255)
+red = (255, 0, 0)
+green = (188, 227, 199)
+
+# Fonts for the game
+score_font = pygame.font.SysFont("arialblack", 20)
+exit_font = pygame.font.Font("freesansbold.ttf", 30)
+
+quit_game = False
+
+while not quit_game:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            quit_game = True
 
 pygame.quit()
 quit()
